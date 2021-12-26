c61aba5f4af2eb2ab5f8438a79bb12a4878684c6
Mohit-Chaudhari <mohit.chaudhari@pubmatic.com>
2021-12-27 00:47:49 +0530

    GRAPH REPRESENTATION - ADJACENCY MATRIX

--- GRAPHS BASICS/graph_representation_in_adjacency_matrix.py
+++ GRAPHS BASICS/graph_representation_in_adjacency_matrix.py
@@ -5,92 +5,259 @@
 
 # GRAPH REPRESENTATION - ADJACENCY MATRIX
 
-class Vertex:
-
-    def __init__(self, node):
-        self.id = node
-        self.visited = False
-
-    def set_vertex_id(self, ids):
-        self.id = ids
-
-    def get_vertex_id(self):
-        return self.id
-
-    def add_neighbour(self, neighbour):
-        G.add_edge(self.id, neighbour)
-
-    def set_visited(self):
-        self.visited = True
-
-
 class Graph:
 
-    def __init__(self, num_vertices, cost=0):
-        self.adj_matrix = [[-1] * num_vertices for _ in range(num_vertices)]
-        self.num_vertices = num_vertices
-        self.vertices = []
-        for i in range(0, num_vertices):
-            new_vertex = Vertex(i)
-            self.vertices.append(new_vertex)
+    def __init__(self, vertices):
+        self._vertices = vertices
+        self._adj_matrix = list()
+        for _ in range(vertices):
+            self._adj_matrix.append([0] * vertices)
 
-    def set_vertex(self, vtx, ids):
-        if 0 <= vtx < self.num_vertices:
-            self.vertices[vtx].set_vertex_id(ids)
+        self._visited = [0] * vertices
 
-    def get_vertex(self, n):
-        for vertex_in in range(0, self.num_vertices):
-            if n == self.vertices[vertex_in].get_vertex_id():
-                return vertex_in
-        return -1
+    def insert_edge(self, u, v, w=1):
+        self._adj_matrix[u][v] = w
 
-    def add_edge(self, frm, to, cost=0):
-        if self.get_vertex(frm) != -1 and self.get_vertex(to) != -1:
-            self.adj_matrix[self.get_vertex(frm)][self.get_vertex(to)] = cost
-            # FOR DIRECTED GRAPH - DO NOT ADD THIS
-            self.adj_matrix[self.get_vertex(to)][self.get_vertex(frm)] = cost
+    def remove_edge(self, u, v):
+        self._adj_matrix[u][v] = 0
 
-    def print_matrix(self):
-        for u in range(0, self.num_vertices):
-            row = []
-            for v in range(0, self.num_vertices):
-                row.append(self.adj_matrix[u][v])
-            print(row)
+    def exist_edge(self, u, v):
+        return self._adj_matrix[u][v] != 0
 
-    def get_edges(self):
-        edges = []
-        for v in range(0, self.num_vertices):
-            for u in range(0, self.num_vertices):
-                if self.adj_matrix[u][v] != -1:
-                    vid = self.vertices[v].get_vertex_id()
-                    wid = self.vertices[u].get_vertex_id()
-                    edges.append((vid, wid, self.adj_matrix[u][v]))
-        return edges
+    def vertex_count(self):
+        return self._vertices
 
-    def get_vertices(self):
-        vertices = []
-        for vertex_in in range(0, self.num_vertices):
-            vertices.append(self.vertices[vertex_in].get_vertex_id())
-        return vertices
+    def edge_count(self):
+        count = 0
+        for u in range(self._vertices):
+            for v in range(self._vertices):
+                if self._adj_matrix[u][v] != 0:
+                    count += 1
+        return count
+
+    def vertices(self):
+        for i in range(self._vertices):
+            print(i, end=" ")
+        print()
+
+    def edges(self):
+        for u in range(self._vertices):
+            for v in range(self._vertices):
+                if self._adj_matrix[u][v] != 0:
+                    print(u, "--", v)
+
+    def outdegree(self, v):
+        count = 0
+        for j in range(self._vertices):
+            if self._adj_matrix[v][j] != 0:
+                count += 1
+        return count
+
+    def indegree(self, v):
+        count = 0
+        for j in range(self._vertices):
+            if self._adj_matrix[j][v] != 0:
+                count += 1
+        return count
+
+    def display_adj_matrix(self):
+        # print(self._adj_matrix)
+        for i in range(self._vertices):
+            print(self._adj_matrix[i])
+
+    def bfs(self, s):
+        i = s
+        q = list()
+        q.append(i)
+        print(i, end=" ")
+
+        while q:
+            i = q.pop(0)
+            self._visited[i] = 1
+
+            for v in range(self._vertices):
+                if self._adj_matrix[i][v] != 0 and self._visited[v] == 0:
+                    self._visited[v] = 1
+                    print(v, end=" ")
+                    q.append(v)
+
+    def dfs(self, s):
+        if self._visited[s] == 0:
+            print(s, end=" ")
+            self._visited[s] = 1
+            for j in range(self._vertices):
+                if self._adj_matrix[s][j] == 1 and self._visited[j] == 0:
+                    self.dfs(j)
 
 
-if __name__ == "__main__":
-    G = Graph(6)
-    G.set_vertex(0, 'a')
-    G.set_vertex(1, 'b')
-    G.set_vertex(2, 'c')
-    G.set_vertex(3, 'd')
-    G.set_vertex(4, 'e')
-    G.set_vertex(5, 'f')
-    print(" - Graph data : ")
-    G.add_edge('a', 'e', 10)
-    G.add_edge('a', 'c', 20)
-    G.add_edge('c', 'b', 30)
-    G.add_edge('b', 'e', 40)
-    G.add_edge('e', 'd', 50)
-    G.add_edge('f', 'e', 60)
-    G.print_matrix()
-    print(" - GET EDGES : ")
-    print(G.get_edges())
-    print(" - GET VERTICES : ")
-    print(G.get_vertices())
+def undirected_graph():
+    G = Graph(4)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    print("\n - Vertex count : ", G.vertex_count())
+    G.insert_edge(0, 1)
+    G.insert_edge(0, 2)
+    G.insert_edge(1, 0)
+    G.insert_edge(1, 2)
+    G.insert_edge(2, 0)
+    G.insert_edge(2, 1)
+    G.insert_edge(2, 3)
+    G.insert_edge(3, 2)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    G.edges()
+    print("\n - Edge between 1 - 3 : ", G.exist_edge(1, 3))
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+    print("\n - InDegree of 2 : ", G.indegree(2))
+    print("\n - OutDegree of 2 : ", G.outdegree(2))
+    G.remove_edge(1, 2)
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+
+
+def weighted_undirected_graph():
+    G = Graph(4)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    print("\n - Vertex count : ", G.vertex_count())
+    G.insert_edge(0, 1, 26)
+    G.insert_edge(0, 2, 16)
+    G.insert_edge(1, 0, 26)
+    G.insert_edge(1, 2, 12)
+    G.insert_edge(2, 0, 16)
+    G.insert_edge(2, 1, 12)
+    G.insert_edge(2, 3, 8)
+    G.insert_edge(3, 2, 8)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    G.edges()
+    print("\n - Edge between 1 - 3 : ", G.exist_edge(1, 3))
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+    print("\n - InDegree of 2 : ", G.indegree(2))
+    print("\n - OutDegree of 2 : ", G.outdegree(2))
+    G.remove_edge(1, 2)
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+
+
+def directed_graph():
+    G = Graph(4)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    print("\n - Vertex count : ", G.vertex_count())
+    G.insert_edge(0, 1)
+    G.insert_edge(0, 2)
+    G.insert_edge(1, 2)
+    G.insert_edge(2, 3)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    G.edges()
+    print("\n - Edge between 1 - 3 : ", G.exist_edge(1, 3))
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+    print("\n - InDegree of 2 : ", G.indegree(2))
+    print("\n - OutDegree of 2 : ", G.outdegree(2))
+    G.remove_edge(1, 2)
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+
+
+def weighted_directed_graph():
+    G = Graph(4)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    print("\n - Vertex count : ", G.vertex_count())
+    G.insert_edge(0, 1, 26)
+    G.insert_edge(0, 2, 16)
+    G.insert_edge(1, 2, 12)
+    G.insert_edge(2, 3, 8)
+    G.display_adj_matrix()
+    print("\n - Edges count : ", G.edge_count())
+    G.edges()
+    print("\n - Edge between 1 - 3 : ", G.exist_edge(1, 3))
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+    print("\n - InDegree of 2 : ", G.indegree(2))
+    print("\n - OutDegree of 2 : ", G.outdegree(2))
+    G.remove_edge(1, 2)
+    print("\n - Edge between 1 - 2 : ", G.exist_edge(1, 2))
+
+
+def breadth_first_search():
+    G = Graph(7)
+    G.insert_edge(0, 1)
+    G.insert_edge(0, 6)
+    G.insert_edge(0, 5)
+    G.insert_edge(1, 0)
+    G.insert_edge(1, 6)
+    G.insert_edge(1, 2)
+    G.insert_edge(1, 5)
+    G.insert_edge(2, 6)
+    G.insert_edge(2, 4)
+    G.insert_edge(2, 3)
+    G.insert_edge(3, 4)
+    G.insert_edge(4, 2)
+    G.insert_edge(4, 5)
+    G.insert_edge(5, 2)
+    G.insert_edge(5, 3)
+    G.insert_edge(6, 3)
+    G.display_adj_matrix()
+    print("\n - InDegree of 5 : ", G.indegree(5))
+    print("\n - OutDegree of 5 : ", G.outdegree(5))
+    print("\n - BFS FROM NODE 1 : ", end=" ")
+    G.bfs(1)
+    print()
+
+
+def depth_first_search():
+    G = Graph(7)
+    G.insert_edge(0, 1)
+    G.insert_edge(0, 6)
+    G.insert_edge(0, 5)
+    G.insert_edge(1, 0)
+    G.insert_edge(1, 6)
+    G.insert_edge(1, 2)
+    G.insert_edge(1, 5)
+    G.insert_edge(2, 6)
+    G.insert_edge(2, 4)
+    G.insert_edge(2, 3)
+    G.insert_edge(3, 4)
+    G.insert_edge(4, 2)
+    G.insert_edge(4, 5)
+    G.insert_edge(5, 2)
+    G.insert_edge(5, 3)
+    G.insert_edge(6, 3)
+    G.display_adj_matrix()
+    print("\n - InDegree of 5 : ", G.indegree(5))
+    print("\n - OutDegree of 5 : ", G.outdegree(5))
+    print("\n - DFS FROM NODE 1 : ", end=" ")
+    G.dfs(1)
+    print()
+
+
+if __name__ == '__main__':
+    print("-" * 20, " UNDIRECTED GRAPH ", "-" * 20)
+    undirected_graph()
+    print("-" * 20, " UNDIRECTED GRAPH END ", "-" * 20)
+    print("\n")
+
+    print("-" * 20, " WEIGHTED UNDIRECTED GRAPH ", "-" * 20)
+    weighted_undirected_graph()
+    print("-" * 20, " WEIGHTED UNDIRECTED GRAPH END ", "-" * 20)
+    print("\n")
+
+    print("-" * 20, " DIRECTED GRAPH ", "-" * 20)
+    directed_graph()
+    print("-" * 20, " DIRECTED GRAPH END ", "-" * 20)
+    print("\n")
+
+    print("-" * 20, " WEIGHTED DIRECTED GRAPH ", "-" * 20)
+    weighted_directed_graph()
+    print("-" * 20, " WEIGHTED DIRECTED GRAPH END ", "-" * 20)
+    print("\n")
+
+    print("-" * 20, " BREADTH FIRST SEARCH ", "-" * 20)
+    breadth_first_search()
+    print("-" * 20, " BREADTH FIRST SEARCH END ", "-" * 20)
+    print("\n")
+
+    print("-" * 20, " DEPTH FIRST SEARCH ", "-" * 20)
+    depth_first_search()
+    print("-" * 20, " DEPTH FIRST SEARCH END ", "-" * 20)
+    print("\n")
+
